c6918618b25b3fdc1a590e84c75a6b5e08685072
Andersen <andersen.huang@goldnetit.com>
2020-01-09 13:15:11 +0800

    add unix time

--- component.js
+++ component.js
@@ -6,6 +6,8 @@ let tpl = `
     concat string <input type="text" v-model="firstStr" > <br>
     unix timestamp <input type="text" v-model="unix" > <br>
     <button @click="handleGenerateUnixTime">Gen Unix Time</button>
+    <button @click="handleReductionUnixTime">Reduction Unix Time</button> <br>
+    transfer <input type="text" v-model="now" >
 </div>
 `;
 
@@ -16,7 +18,8 @@ Vue.component('ex', {
         return {
             def: 'this is component',
             firstStr: '',
-            unix: ''
+            unix: '',
+            now: ''
         }
     },
     created() {
@@ -29,6 +32,14 @@ Vue.component('ex', {
         handleGenerateUnixTime() {
             this.unix = this.firstStr + (Math.floor(Date.now() / 1000)).toString();
             console.log(this.unix);
+        },
+        handleReductionUnixTime() {
+            let myRe = /\d{10,}/gm;
+            let str = this.unix;
+            let result = myRe.exec(str);
+            console.log(result);
+            let now = new Date(result[0] * 1000).Format("yyyy-M-d h:m:s");
+            this.now = now;
         }
     }
 })
