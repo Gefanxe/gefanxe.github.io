ee7a82d7d07716c2987a1cd298550b25807e0d08
Andersen <andersen.huang@goldnetit.com>
2020-08-05 13:08:50 +0800

    update

--- assets/tipsapp.js
+++ assets/tipsapp.js
@@ -1,11 +1,50 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let vue = new Vue({
-      el: "#app",
-      data: {
-        message: "Hello World!"
+// https://picsum.photos/id/1/344/194
+
+document.addEventListener("DOMContentLoaded", function () {
+  let vue = new Vue({
+    el: "#app",
+    data: {
+      message: "Hello World!",
+      tips_list: {
+        "data": [
+          {
+            "index": 0,
+            "title": "讀取中...",
+            "content": "讀取中...",
+            "img": "/assets/Loading.png"
+          }
+        ]
       },
-      created: function() {
-        console.log("created event fired in app.");
+      loadTips: false
+    },
+    created: function () {
+      console.log("created !!");
+
+      this.loadJsonData(this.changeLoadTipsStatus);
+    },
+    mounted: function() {
+      console.log("mounted!");
+    },
+    methods: {
+      handleChangeCard() {
+        console.log('change !!!');
+      },
+      handleTest() {
+
+      },
+      // 讀取提示Json資料
+      loadJsonData(cb) {
+        let self = this;
+        fetch('/tips_list.json').then(function(response) {
+          return response.json();
+        }).then(function(myJson) {
+          self.tips_list.data = myJson.data;
+          if (cb) cb();
+        });
+      },
+      changeLoadTipsStatus() {
+        this.loadTips = true;
       }
-    });
+    }
   });
+});
--- assets/tipscomponent.js
+++ assets/tipscomponent.js
@@ -3,81 +3,70 @@ let tpl = `
     <v-list-item>
         <v-list-item-avatar color="grey" v-text="index"></v-list-item-avatar>
         <v-list-item-content>
-            <v-list-item-title class="headline" v-text="title">Our Changing Planet</v-list-item-title>
+            <v-list-item-title class="headline" v-text="title"></v-list-item-title>
         </v-list-item-content>
     </v-list-item>
 
-    <v-img src="https://cdn.vuetifyjs.com/images/cards/mountain.jpg" height="194"></v-img>
+    <v-img v-bind:src="img" height="194"></v-img>
 
-<v-card-text>
-    Visit ten places on our planet that are undergoing the biggest changes today.
-</v-card-text>
+<v-card-text v-text="content"></v-card-text>
 
 <v-card-actions>
-    <v-btn
-    text
-    color="deep-purple accent-4"
-    >
-    Read
-    </v-btn>
-    <v-btn
-    text
-    color="deep-purple accent-4"
-    >
-    Bookmark
-    </v-btn>
+    <v-btn @click="handleClick">Read</v-btn>
     <v-spacer></v-spacer>
     <v-btn icon>
-    <v-icon>mdi-heart</v-icon>
+        <v-icon>mdi-heart</v-icon>
     </v-btn>
     <v-btn icon>
-    <v-icon>mdi-share-variant</v-icon>
+        <v-icon>mdi-share-variant</v-icon>
     </v-btn>
 </v-card-actions>
 </v-card>
 `;
 
-Vue.component('tip-cards', {
-    props: {
-        tip: {
-            type: Object,
-            default: function() {
-                return {
-                    index: 99,
-                    title: '提示卡標題',
-                    content: '提示卡內容描述',
-                    img: 'https://cdn.vuetifyjs.com/images/cards/mountain.jpg'
-                }
-            }
-        }
-    },
-    template: tpl,
-    data() {
+Vue.component("tip-cards", {
+  props: {
+    tip: {
+      type: Object,
+      default: function () {
         return {
-            index: this.tip.index,
-            title: this.tip.title,
-            content: this.tip.content,
-            img: this.tip.img
-        }
+          index: 99,
+          title: "提示卡標題",
+          content: "提示卡內容描述",
+          img: "https://cdn.vuetifyjs.com/images/cards/mountain.jpg",
+        };
+      },
     },
-    created() {
-        console.log('component created!');
+  },
+  computed: {
+    index: {
+      get() {
+        return this.tip.index;
+      },
     },
-    methods: {
-        // handleClick() {
-        //     console.log('button click!');
-        // },
-        // handleGenerateUnixTime() {
-        //     this.unix = this.firstStr + (Math.floor(Date.now() / 1000)).toString();
-        //     console.log(this.unix);
-        // },
-        // handleReductionUnixTime() {
-        //     let myRe = /\d{10,}/gm;
-        //     let str = this.unix;
-        //     let result = myRe.exec(str);
-        //     console.log(result);
-        //     let now = new Date(result[0] * 1000).Format("yyyy-M-d h:m:s");
-        //     this.now = now;
-        // }
+    title: {
+      get() {
+        return this.tip.title;
+      },
+    },
+    content: {
+      get() {
+        return this.tip.content;
+      },
+    },
+    img: {
+      get() {
+        return this.tip.img;
+      },
+    },
+  },
+  template: tpl,
+  created() {
+    console.log("component created!");
+  },
+  methods: {
+    handleClick() {
+      console.log('compoment data:', this.title);
     }
-})
+  },
+});
