7b95d115c63c37c3b23355918d2c0261c45afba8
Andersen <actfire@gmail.com>
2021-10-05 18:31:29 +0800

    update

--- assets/component.js
+++ assets/component.js
@@ -7,7 +7,11 @@ let tpl = `
     unix timestamp <input type="text" v-model="unix" size="30"> <br>
     <button @click="handleGenerateUnixTime">Gen Unix Time</button>
     <button @click="handleReductionUnixTime">Reduction Unix Time</button> <br>
-    transfer <input type="text" v-model="now" >
+    transfer <input type="text" v-model="now" > <br>
+    quadratic bezier points Convert to xy json value <br>
+    <input type="text" v-model="brPoint" size="80"> <span style="color: red;">{{ errMsg }}</span> <br>
+    <button @click="brTransfer">transfer</button>
+    result:<pre>{{ brResult }}</pre>
 </div>
 `;
 
@@ -19,13 +23,40 @@ Vue.component('ex', {
             def: 'this is component',
             firstStr: '',
             unix: '',
-            now: ''
+            now: '',
+            brPoint: '',
+            errMsg: '',
+            brResult: ''
         }
     },
     created() {
         console.log('component created!');
     },
     methods: {
+        brTransfer() {
+            const self = this;
+            if (self.brPoint !== '') {
+                let brArr = self.brPoint.split(',');
+                let c = brArr.length;
+                let p = (c / 2) % 5;
+                if (p === 0 && (c / 2) >= 3) {
+                    let resultObj = '';
+                    let point = {};
+                    brArr.forEach((item, idx, array) => {
+                        if ((idx % 2) === 0) {
+                            point.x = item;
+                        } else {
+                            point.y = item;
+                            resultObj += `{x: ${point.x}, y: ${point.y}}` + ",\n";
+                            point = {};
+                        }
+                    });
+                    self.brResult = "[\n" + resultObj + "]";
+                } else {
+                    self.errMsg = '座標點必須為奇數(3個點以上)';
+                }
+            }
+        },
         handleClick() {
             console.log('button click!');
         },
